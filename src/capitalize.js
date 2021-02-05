67ee90c46cf87a3a33ccd42f33d07c8cfa25358c
Grigory Sadovskiy <sadovskiy.grigory@yandex.ru>
2021-02-05 17:07:49 +0300

    added assert test module

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,8 +1,10 @@
-export const capitalize = (text) => {
+function capitalize(text) {
   if (text === "") {
     return "";
   }
   const firstSymbol = text[0].toUpperCase();
   const restSubstring = text.slice(1);
   return `${firstSymbol}${restSubstring}`;
-};
+}
+
+export default capitalize;
